7ae83c2c492692048b2ee63bb79bc1040664a8f3
Marcelo Cabral Ghilardi <kabrau@gmail.com>
2019-10-09 23:25:22 -0300

    Video Dataset

---
+++ ExportAnnoationsOfVideos.bat
@@ -0,0 +1,15 @@
+cd E:\GitHub\PyImageRoi\source
+
+    python ExportToPascal.py -p F:\datasets\door_stair\TestingVideos\door-1\Images -a F:\datasets\door_stair\TestingVideos\door-1\Annotations -className opened_door closed_door elevator_door 
+    python ExportToPascal.py -p F:\datasets\door_stair\TestingVideos\door-2\Images -a F:\datasets\door_stair\TestingVideos\door-2\Annotations -className opened_door closed_door elevator_door 
+    python ExportToPascal.py -p F:\datasets\door_stair\TestingVideos\door-3\Images -a F:\datasets\door_stair\TestingVideos\door-3\Annotations -className opened_door closed_door elevator_door 
+    python ExportToPascal.py -p F:\datasets\door_stair\TestingVideos\door-4\Images -a F:\datasets\door_stair\TestingVideos\door-4\Annotations -className opened_door closed_door elevator_door 
+    python ExportToPascal.py -p F:\datasets\door_stair\TestingVideos\door-5\Images -a F:\datasets\door_stair\TestingVideos\door-5\Annotations -className opened_door closed_door elevator_door 
+    python ExportToPascal.py -p F:\datasets\door_stair\TestingVideos\door-6\Images -a F:\datasets\door_stair\TestingVideos\door-6\Annotations -className opened_door closed_door elevator_door 
+
+    python ExportToPascal.py -p F:\datasets\door_stair\TestingVideos\stair-1\Images -a F:\datasets\door_stair\TestingVideos\stair-1\Annotations -className ascending_stair descending_stair
+    python ExportToPascal.py -p F:\datasets\door_stair\TestingVideos\stair-2\Images -a F:\datasets\door_stair\TestingVideos\stair-2\Annotations -className ascending_stair descending_stair
+    python ExportToPascal.py -p F:\datasets\door_stair\TestingVideos\stair-3\Images -a F:\datasets\door_stair\TestingVideos\stair-3\Annotations -className ascending_stair descending_stair
+    python ExportToPascal.py -p F:\datasets\door_stair\TestingVideos\stair-4\Images -a F:\datasets\door_stair\TestingVideos\stair-4\Annotations -className ascending_stair descending_stair
+
+cd E:/GitHub/WACV-2020
